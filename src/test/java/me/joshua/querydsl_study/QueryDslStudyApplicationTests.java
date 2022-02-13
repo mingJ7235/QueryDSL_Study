5bfa1368fc314e3b7c8946857f5c9c122cc11a0d
김민재 <joshuara7235@gmail.com>
2022-02-13 20:03:11 +0900

    init push

---
+++ src/test/java/me/joshua/querydsl_study/QueryDslStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package me.joshua.querydsl_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueryDslStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
