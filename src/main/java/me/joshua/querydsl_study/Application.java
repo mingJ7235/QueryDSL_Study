0f8e49e4130258b8a64fa1a9041b8201f7a85b52
김민재 <joshuara7235@gmail.com>
2022-02-13 20:33:19 +0900

    Basic setting for studying QueryDsl

--- src/main/java/me/joshua/querydsl_study/Application.java
+++ src/main/java/me/joshua/querydsl_study/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class QueryDslStudyApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(QueryDslStudyApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
---
+++ src/test/java/me/joshua/querydsl_study/ApplicationTests.java
@@ -0,0 +1,38 @@
+package me.joshua.querydsl_study;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import me.joshua.querydsl_study.entity.Hello;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import static me.joshua.querydsl_study.entity.QHello.hello;
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class ApplicationTests {
+
+    @Autowired
+    EntityManager em;
+
+    @Test
+    void contextLoads() {
+        Hello hello2 = new Hello();
+        em.persist(hello2);
+
+        JPAQueryFactory query = new JPAQueryFactory(em);
+
+        Hello result = query
+                .selectFrom(hello)
+                .fetchOne();
+
+        assertThat(result).isEqualTo(hello2);
+        assertThat(result.getId()).isEqualTo(hello2.getId());
+    }
+
+}
